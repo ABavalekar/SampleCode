a5ab97e36d18219125af5f5dd6e8c3604d4e0565
Ajit <Ajit@LAPTOP-1KU50D6P>
2020-08-12 19:59:47 +0530

    Commit done

---
+++ Sample/src/com/something/product/DemoTest.java
@@ -0,0 +1,27 @@
+package com.something.product;
+
+public class DemoTest {
+	
+	
+	
+	static int add(int a, int b)
+	{
+		int c = a + b;
+		return c;
+		
+	}
+	
+	static int sub(int a, int b)
+	{
+		int c= a-b;
+		return c;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		add(10,20);
+		sub(20,10);
+	}
+
+}
